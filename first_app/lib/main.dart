c359d744331222bc71a536c68f4058e29b1cc759
Pranav <pranavbansal777@gmail.com>
2020-03-29 02:51:38 +0530

    Added Column in the body

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -12,7 +10,23 @@ class MyApp extends StatelessWidget {
           title: Text('New App'),
           backgroundColor: Color(0xFF45B39D),
         ),
-        body: Text('Body of the App'),
+        body: Column(
+          children: [
+            Text('many widgets displayed'),
+            RaisedButton(
+              child: Text('Button 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Button 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Button 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
