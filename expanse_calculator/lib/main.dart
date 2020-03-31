9838c3743522eeb80e1389a48cc9a779e4ba84d9
Pranav <pranavbansal777@gmail.com>
2020-03-31 17:01:08 +0530

    Added Blueprint for the transaction

--- expanse_calculator/lib/main.dart
+++ expanse_calculator/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expanse_calculator/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -14,14 +15,42 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Eggs',
+      amount: 15,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Milk',
+      amount: 20,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: 100,
+            child: Card(
+              child: Text('Test Data'),
+            ),
+          ),
+          Container(
+            width: 100,
+            child: Card(
+              child: Text('TX data'),
+            ),
+          ),
+        ],
       ),
     );
   }
