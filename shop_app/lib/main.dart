b1d4ec2423d74e89aeed49e0cd990b2a4b6cb486
Pranav Bansal <pranavbansal777@gmail.com>
2020-07-04 19:35:02 +0530

    Logging in working

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import './screens/orders_screen.dart';
 import './providers/orders.dart';
+import './providers/auth.dart';
 import './screens/product_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
         ChangeNotifierProvider.value(
           value: Products(),
         ),
