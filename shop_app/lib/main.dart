fa525c64b7cebdbd246f07bb87f18e57eb6e85c6
Pranav Bansal <pranavbansal777@gmail.com>
2020-07-06 02:07:06 +0530

    Added custom Route Transition and done with this application

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -13,6 +13,7 @@ import './screens/cart_screen.dart';
 import './screens/userProducts_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -48,10 +49,13 @@ class MyApp extends StatelessWidget {
         builder: (context, auth, _) => MaterialApp(
           title: 'Shop App',
           theme: ThemeData(
-            primarySwatch: Colors.purple,
-            accentColor: Colors.deepOrange,
-            fontFamily: 'Lato',
-          ),
+              primarySwatch: Colors.purple,
+              accentColor: Colors.deepOrange,
+              fontFamily: 'Lato',
+              pageTransitionsTheme: PageTransitionsTheme(builders: {
+                TargetPlatform.android: CustomPageTransitionBuilder(),
+                TargetPlatform.iOS: CustomPageTransitionBuilder(),
+              })),
           home: auth.isAuth
               ? ProductOverviewScreen()
               : FutureBuilder(
