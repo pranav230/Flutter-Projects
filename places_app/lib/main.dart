806a216e976bdbd10c83f608e485edc8449a540b
Pranav Bansal <pranavbansal777@gmail.com>
2020-10-08 15:30:16 +0530

    Added the basic setup

--- places_app/lib/main.dart
+++ places_app/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'provider/great_places.dart';
+import 'screens/place_list_screen.dart';
+import 'screens/add_place_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,13 +13,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo ,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
       ),
-      home: ,
     );
   }
 }
